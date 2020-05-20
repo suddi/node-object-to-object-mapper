43b08fcdac24a3f4aace96e76da3d92c7971aad6
suddi <suddir@gmail.com>
2020-05-20 11:44:57 +0200

    Updaged scripts/publish-docs.sh

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -6,5 +6,12 @@ function move_files() {
     cp README.md docs/README.md
 }
 
+function remove_files() {
+    rm -rf docs/.circleci
+    rm docs/.gitignore
+    rm docs/README.md
+}
+
 move_files
-git subtree split --branch gh-pages --prefix docs
+git subtree split --branch gh-pages --prefix docs/
+remove_files
