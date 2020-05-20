7cab1c64db5d4a77f4672239b344ba8e138eb3d1
suddi <suddir@gmail.com>
2020-05-20 11:41:30 +0200

    Updaged scripts/publish-docs.sh

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 function move_files() {
-    cp -r docs gh-pages
-    cp -r .circleci gh-pages/.circleci
-    cp .gitignore gh-pages/.gitignore
-    cp README.md gh-pages/README.md
+    cp -r .circleci docs/.circleci
+    cp .gitignore docs/.gitignore
+    cp README.md docs/README.md
 }
 
 move_files
-git subtree split --branch gh-pages --prefix gh-pages
+git subtree split --branch gh-pages --prefix docs
